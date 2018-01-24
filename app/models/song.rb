07b4babf288712f6a9b0659ddca06d59ee3646e5
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-24 12:38:53 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by(:name => name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
@@ -12,7 +12,7 @@ class Song < ActiveRecord::Base
   end
 
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(:name => name)
+    self.genre = Genre.find_or_create_by(name: name)
   end
 
   def genre_name
